01eac896eac0709a005f3c1242d9e59d8d5638d0
Arthur Savage <alerc@meta.ua>
2016-09-30 11:40:29 +0300

    Main Program in Project for GO
    Main Program in Project for GO

---
+++ main.go
@@ -0,0 +1,29 @@
+// Main programm
+package main
+
+import (
+   "fmt"
+   "time"
+   "sync"
+)
+
+
+// 
+// Main programm
+// 
+func main() {
+
+}
+
+
+
+//
+// Name        :  
+// Date        :
+// Author      :
+// Description :
+//
+function OtherFunction(){
+
+}
+
